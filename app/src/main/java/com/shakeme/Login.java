bf256365f60524fa255a0d442e3f9d27ff748bd1
Ravindu <ravinduiroshana123@live.com>
2017-03-31 19:31:39 +0530

    update

--- app/src/main/java/com/shakeme/Login.java
+++ app/src/main/java/com/shakeme/Login.java
@@ -1,13 +1,30 @@
 package com.shakeme;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Login extends AppCompatActivity {
+
+    private ImageView imgLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        imgLoginBtn = (ImageView) findViewById(R.id.img_LoginBtn);
+
+        imgLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        SignUp.class));
+            }
+        });
     }
+
+
 }
