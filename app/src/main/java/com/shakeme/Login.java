d03f47dcd8fadf566fb905234e55290c1f65d6ea
Ravindu <ravinduiroshana123@live.com>
2017-04-01 20:21:45 +0530

    home

--- app/src/main/java/com/shakeme/Login.java
+++ app/src/main/java/com/shakeme/Login.java
@@ -21,7 +21,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),
-                        SignUp.class));
+                        Home.class));
             }
         });
     }
